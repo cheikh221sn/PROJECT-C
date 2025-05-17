57423b1b7317caf7df81cf974ff18366674360d6
SilentX <fallcheikhtidiane99@gmail.com>
2025-05-17 13:18:18 +0000

    ajoutfonction

--- projet.c
+++ projet.c
@@ -11,7 +11,7 @@ typedef struct {
     int eff;
 } classe;
 
-// Ajouter une classe et enregistrement dans fichier
+// Ajouter une classe et enregistrement dans fichiers
 void AjouterClasse(classe classes[], int *nbClasses) {
     classe nouvelle;
     printf("Code : ");
